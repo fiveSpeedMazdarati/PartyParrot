ef0e48a0bbab62dbeff356b3a393860113407e7e
Heidi Aandahl <hlaandahl@gmail.com>
2019-04-13 15:01:56 -0500

    changes services paths per convention

--- parrot/src/main/java/com/parrotparty/ParrotConfig.java
+++ parrot/src/main/java/com/parrotparty/ParrotConfig.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 //Defines the base URI for all resource URIs.
-@ApplicationPath("/parrots")
+@ApplicationPath("/services")
 
 //The java class declares root resource and provider classes
 public class ParrotConfig extends Application {
--- parrot/src/main/java/com/parrotparty/ParrotService.java
+++ parrot/src/main/java/com/parrotparty/ParrotService.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@Path("/parrot")
+@Path("/parrots")
 public class ParrotService {
 
     ObjectMapper mapper;
